5960caa562c78064a5a48e133f246b42e341af1d
sabarim6369 <151687270+sabarim6369@users.noreply.github.com>
2024-01-17 14:14:32 +0530

    Update arrayscharachters.c

--- arrayscharachters.c
+++ arrayscharachters.c
@@ -1,23 +1,8 @@
-hbh
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-fgfgfgf
-
-gregrhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhe
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-grrrrrr
-n
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
-deff
-q
-q
-q
-q
-q
-q
-q
+git clone link
+cd
+git init
+git add filename
+git commit -m "duhbsdibv"
+git push origin main
+git pull
+(if we have two folders.example sample1 and sample2.we are working under sample 1 and we need to got to sample2 then give cd .. and after that give cd sample2)
