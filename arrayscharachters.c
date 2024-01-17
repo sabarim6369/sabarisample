c263ce6b6dbfb9b38adec06bd0201f803bb6aa7a
sabarim6369 <151687270+sabarim6369@users.noreply.github.com>
2024-01-17 14:05:33 +0530

    Add files via upload

---
+++ arrayscharachters.c
@@ -0,0 +1,23 @@
+hbh
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+fgfgfgf
+
+gregrhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhe
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+grrrrrr
+n
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+cd "c:\Users\User\Desktop\c program\" ; if ($?) { gcc rg.c -o rg } ; if ($?) { .\rg }
+deff
+q
+q
+q
+q
+q
+q
+q
